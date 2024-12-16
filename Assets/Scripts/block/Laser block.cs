3596a074731d8a3fc50e41c31541ed89755de6f9
112vasd <a49458447@gmail.com>
2024-12-16 13:26:35 +0900

    Power

--- Assets/Scripts/block/Laser block.cs
+++ Assets/Scripts/block/Laser block.cs
@@ -4,9 +4,15 @@ using System;
 public class Laserblock : MonoBehaviour
 {
     public bool shouldRotate = false;
-    public bool chek = false;
+    public GameObject g;
+
+    public float speed = 5.0f;  // 이동 속도
     void Update()
     {
+        float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime; // A와 D 키, 좌우 화살표에 반응
+        float moveZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;   // W와 S 키, 상하 화살표에 반응
+        transform.Translate(moveX, moveZ, 0);
+
         float rayLength = 5.0f;  // 레이캐스트의 길이
         int layerMask = LayerMask.GetMask("sss");  // 레이캐스트가 반응할 레이어 마스크
 
@@ -38,6 +44,14 @@ public class Laserblock : MonoBehaviour
                     {
                         Debug.Log("Found TragetBlock2, resetting rotation.");
                         shouldRotate = false;  // 두 번째 블록에 충돌 시 회전 리셋
+                    }
+                    else if (hit.collider.name == "Power")
+                    {
+                        Collider2D collider = g.GetComponent<Collider2D>();
+                        if (collider != null)
+                        {
+                            collider.isTrigger = !collider.isTrigger;
+                        }
                     }
                 }
             }
