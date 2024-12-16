5915ea57368b11a25efa59ac92de425326cf4be1
24기구원 <koowonqwe2@gmail.com>
2024-12-16 12:44:08 +0900

    stage

---
+++ Assets/Scripts/MainScreen/StageBtnManager.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageBtnManager : MonoBehaviour
+{
+    public void stage11()
+    {
+        SceneManager.LoadScene("1.1");
+    }
+    public void stage12()
+    {
+        SceneManager.LoadScene("1.2");
+    }
+    public void stage13()
+    {
+        SceneManager.LoadScene("1.3");
+    }
+    public void stage14()
+    {
+        SceneManager.LoadScene("1.4");
+    }
+    public void stage15()
+    {
+        SceneManager.LoadScene("1.5");
+    }
+    public void stage16()
+    {
+        SceneManager.LoadScene("1.6");
+    }
+
+    public void stage21()
+    {
+        SceneManager.LoadScene("2.1");
+    }
+    public void stage22()
+    {
+        SceneManager.LoadScene("2.2");
+    }
+    public void stage23()
+    {
+        SceneManager.LoadScene("2.3");
+    }
+    public void stage24()
+    {
+        SceneManager.LoadScene("2.4");
+    }
+    public void stage25()
+    {
+        SceneManager.LoadScene("2.5");
+    }
+    public void stage26()
+    {
+        SceneManager.LoadScene("2.6");
+    }
+
+    public void stage31()
+    {
+        SceneManager.LoadScene("3.1");
+    }
+    public void stage32()
+    {
+        SceneManager.LoadScene("3.2");
+    }
+    public void stage33()
+    {
+        SceneManager.LoadScene("3.3");
+    }
+    public void stage34()
+    {
+        SceneManager.LoadScene("3.4");
+    }
+    public void stage35()
+    {
+        SceneManager.LoadScene("3.5");
+    }
+    public void stage36()
+    {
+        SceneManager.LoadScene("3.6");
+    }
+}
